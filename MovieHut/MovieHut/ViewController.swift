1ae2a5927eb61904994a119766328e913fa95264
waqas <1waqas.k@gmail.com>
2023-01-28 02:02:48 +0500

    initial commit

---
+++ MovieHut/MovieHut/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieHut
+//
+//  Created by Waqas Khan on 1/28/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
